0584e48d48b2d64f42a6a1bab012b477e21f3c22
vicky-gupta <vicky986832@gmail.com>
2025-10-22 17:08:32 +0200

    Final Solution

---
+++ Project_1_Box_Detection/project_1.py
@@ -0,0 +1,10 @@
+# #import matplotlib.pyplot as plt
+# #import scipy.io
+
+# data = scipy.io.loadmat("data\example1linect.mat")
+
+# print(data.keys())
+
+# plt.imshow(data['distance1'])
+# plt.show()
+print("kbjkvfd")
